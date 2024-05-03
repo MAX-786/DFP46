990974e08abffb79eb6400d02bebadc631441d64
CSG <csg4786@gmail.com>
2024-05-03 23:53:42 +0530

    Add Base Structure

---
+++ client/src/components/homeData.js
@@ -0,0 +1,20 @@
+export const data = [
+  {
+    image: "../images/card/img1.png",
+    heading: "Learn Chapters",
+    content:
+      "Learning isn't just limited to classes with our practice section, mock tests and lecture notes shared as PDFs for your revision",
+  },
+  {
+    image: "../images/card/img2.png",
+    heading: "Practice and Revise",
+    content:
+      "Learning isn't just limited to lectures, try our practice section, mock tests and offline notes for your revision",
+  },
+  {
+    image: "../images/card/img3.png",
+    heading: "Track your Activity",
+    content:
+      "Learning isn't just limited to classes with our practice section, mock tests and lecture notes shared as PDFs for your revision",
+  },
+];
