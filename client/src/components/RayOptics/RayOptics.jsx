3c93e21c57d3966413e77a774f40acf8f4925546
MAX-786 <hussainmohammad328@gmail.com>
2024-05-07 13:53:45 +0530

    init commit

---
+++ client/src/components/RayOptics/RayOptics.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './RayOptics.css'; // Import CSS file for styling
+
+const RayOptics = () => {
+  return (
+    <div className="website-iframe-container">
+      <iframe
+        src="https://max-786.github.io/website-iframe/"
+        title="My website"
+        className="website-iframe"
+      ></iframe>
+    </div>
+  );
+};
+
+export default RayOptics;
+
